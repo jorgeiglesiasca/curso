25e88768f05a0a26feab314d0069f59ca60f6930
Jorge <jorgeiglesias@gmail.com>
2024-05-19 13:52:27 +0200

    primeros modelos

--- database/migrations/2024_05_18_175820_create_pedidos_table.php
+++ database/migrations/2024_05_18_175820_create_pedidos_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
         Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->codigo();
-            $table->nombre();
-            $table->numpax();
-            $table->telefono();
-            
+            $table->unsignedInteger('codigo')->unique();
+            $table->string('nombre', 255);
+            $table->unsignedInteger('numpax');
+            $table->unsignedInteger('telefono');
+            $table->text('notas')->nullable();
+            $table->boolean('volado')->nullable()->default(false);
+            $table->boolean('exclusivo')->nullable()->default(false);
         });
     }
 
