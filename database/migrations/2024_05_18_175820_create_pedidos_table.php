360918e655ed4abcdbab52f1c53dcce04a22fcfd
Jorge <jorgeiglesias@gmail.com>
2024-05-19 13:33:57 +0200

    migracion inicial

--- database/migrations/2024_05_18_175820_create_pedidos_table.php
+++ database/migrations/2024_05_18_175820_create_pedidos_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->codigo();
+            $table->nombre();
+            $table->numpax();
+            $table->telefono();
+            
         });
     }
 
